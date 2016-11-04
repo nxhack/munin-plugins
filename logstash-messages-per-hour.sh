35d356abdd64658e585e38d537407df9df667728
Hirokazu MORIKAWA <morikw2@gmail.com>
2016-11-04 10:40:39 +0900

    support ES 5.x

--- logstash-messages-per-hour.sh
+++ logstash-messages-per-hour.sh
@@ -23,18 +23,18 @@ MUNIN_PERIOD=5 #minutes
 case $1 in
    config)
         cat <<'EOM'
-graph_title Central Log Messages Per Hour
-graph_vlabel Messages / Hour
-events.label syslog messages
+graph_title Logstash Events Per Hour
+graph_vlabel Events / Hour
+events.label events
 graph_scale no
-graph_category logging
+graph_category logstash
 graph_info The number of events the logstash server handles per hour
 events.info Number of log events per 5 minutes.
 EOM
         exit 0;;
 esac
 
-TOTAL_EVENTS=$(curl -s -k -XGET http://localhost:9200/logstash-`date +%Y.%m.%d`/_search -d '{ "size": 0, "query": { "filtered": { "query": { "match_all": { } }, "filter": { "range": { "@timestamp": { "from": "'`date --date "2 hours ago" +%Y-%m-%dT%H:00:00+01:00`'", "to": "'`date --date "1 hour ago" +%Y-%m-%dT%H:00:00+01:00`'" } } } } }, "from": 0, "sort": { "@timestamp": { "order": "desc" } }}' | grep --only \"hits\"\:\{\"total\"\:[0-9]*,\" | grep -o [0-9]*)
+TOTAL_EVENTS=$(curl -s -k -XGET http://127.0.0.1:9200/logstash-`/bin/date -u --date "1 day ago" +%Y.%m.%d`,logstash-`/bin/date -u +%Y.%m.%d`/_search -d '{ "size": 0, "query": { "bool": { "must": { "match_all": { } }, "filter": { "range": { "@timestamp": { "from": "'`/bin/date -u --date "1 hours ago" +%Y-%m-%dT%H:00:00.000Z`'", "to": "'`/bin/date -u +%Y-%m-%dT%H:00:00.000Z`'" } } } } }, "from": 0, "sort": { "@timestamp": { "order": "desc" } }}' | /bin/grep --only \"hits\"\:\{\"total\"\:[0-9]*,\" | /bin/grep -o [0-9]*)
 
 # Basic check to see if we get any logging...
 if [[ $TOTAL_EVENTS -lt 1 ]]; then
